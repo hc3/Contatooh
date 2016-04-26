45ff44da68034bc12ec376ce493559bce8de8aa1
Eliel <eliel.floyd@bol.com.br>
2016-04-26 11:57:48 -0300

    adicionado o bootstrap e comecamos a implementar um stylo para o site

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,3 +1,6 @@
 angular.module('contatooh').controller('ContatosController',function($scope,$routeParams) {
-  console.log($routeParams.contatoId);
+  $scope.total = 0;
+  $scope.incrementa = function() {
+    $scope.total++;
+  };
 });
--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,11 @@ angular.module('contatooh',['ngRoute'])
     
     $routeProvider.when('/contato/:contatoId',{
       templateUrl:'partials/contato.html',
-      controller:'ContatosController'
+      controller:'ContatoController'
+    });
+    
+    $routeProvider.otherwise({
+      redirectTo:'/contatos'
     });
     
   });
