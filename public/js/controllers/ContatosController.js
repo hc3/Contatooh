998b4de192e656bc6868b70f22a260b447f23b37
Eliel <eliel.floyd@bol.com.br>
2016-04-26 11:21:11 -0300

    pequenas alteracoes estou enviando por partes por que to no trampo rs

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,6 +1,3 @@
-angular.module('contatooh').controller('ContatosController',function($scope) {
-  $scope.total = 0;
-  $scope.incrementa = function() {
-    $scope.total++;
-  }
+angular.module('contatooh').controller('ContatosController',function($scope,$routeParams) {
+  console.log($routeParams.contatoId);
 });
